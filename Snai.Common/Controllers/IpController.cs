f9405e4e8f12e6d53ba944d8b3b3045947d7cba8
小林 刘 <xiaolin-liu@qq.com>
2018-11-23 14:11:04 +0800

    加入 Snai.Common 常用方法项目

---
+++ Snai.Common/Controllers/IpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Snai.Common.Extension;
+
+namespace Snai.Common.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IpController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get(int id)
+        {
+            return HttpContext.GetUserIp();
+        }
+    }
+}
