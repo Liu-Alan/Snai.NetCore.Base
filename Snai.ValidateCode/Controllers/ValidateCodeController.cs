2ef854c42d6351cfc53441a944d4da69bf99cfa0
Liu-Alan <xiaolin-liu@qq.com>
2019-01-23 06:58:07 +0800

    添加验证码输出

---
+++ Snai.ValidateCode/Controllers/ValidateCodeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Snai.ValidateCode.Infrastructure;
+
+namespace Snai.ValidateCode.Controllers
+{
+    public class ValidateCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ValidateCode(string identify = "")
+        {
+            string codeValue = "";
+            var codeImg = ValidateCodeServices.CreateImage(out codeValue,6);
+            codeValue = codeValue.ToUpper();//验证码不分大小写  
+            Response.Body.Dispose();
+            return File(codeImg, @"image/png");
+        }
+    }
+}
